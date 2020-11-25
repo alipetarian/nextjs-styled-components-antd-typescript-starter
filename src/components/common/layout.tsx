3a4f8518910463bbeb50db65e44a433ff529e18a
Muhammad Ali <alipakistani90@gmail.com>
2020-11-25 19:38:17 +0500

    Converted to Typescript, Added Styled Componets, Antd

---
+++ src/components/common/layout.tsx
@@ -0,0 +1,43 @@
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: "Sofia Pro";
+    src: url("//db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.eot"); /* IE9*/
+    src: url("//db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.eot?#iefix") format("embedded-opentype"), /* IE6-IE8 */
+    url("//db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.woff2") format("woff2"), /* chrome firefox */
+    url("//db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.woff") format("woff"), /* chrome firefox */
+    url("//db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.ttf") format("truetype"), /* chrome firefox opera Safari, Android, iOS 4.2+*/
+    url("//db.onlinewebfonts.com/t/9e00143409affcb46a1ae58634aa64be.svg#Sofia Pro") format("svg"); /* iOS 4.1- */
+  }
+
+  body{
+    font-family: 'Sofia Pro'
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#FF3364',
+    secondary: '#FCC163',
+    tertiary: '#76DED7',
+    quaternary: '#083763',
+    dark: '#333333',
+    darkGrey: '#555A65',
+    midGrey: '#8E93A1',
+    lightGrey: '#DEDEE4',
+    bgGrey: '#F5F5F8',
+    white: '#FFFFFF',
+  },
+}
+
+const Layout = ({ children }) => (
+  <>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </>
+)
+
+
+export default Layout
