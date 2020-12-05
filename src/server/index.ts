570fd7676c4811cb2dbc2c1f1de882557e361165
Muhammad Ali <alipakistani90@gmail.com>
2020-12-05 12:34:49 +0500

    Added Graphql Proxy

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,28 @@
 import express, { Request, Response } from 'express';
 import next from 'next';
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
 
+const apiPaths = {
+  '/graphql': {
+    target: 'https://connectin-hasura-backend.herokuapp.com/v1',
+    pathRewrite: {
+      '^/grapqhl': '/graphql',
+    },
+    changeOrigin: true,
+  },
+};
+
 (async () => {
   try {
     await app.prepare();
     const server = express();
+    server.use('/grapqhl', createProxyMiddleware(apiPaths['/graphql']));
     server.all('*', (req: Request, res: Response) => handle(req, res));
     server.listen(port, (err?: any) => {
       if (err) throw err;
